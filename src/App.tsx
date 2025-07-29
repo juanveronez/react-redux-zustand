c0ad8e59ee05e03ec6e078c2395f5b154a4eeb9d
Juan Veronez <juan.monteirov@gmail.com>
2025-07-29 11:12:04 -0300

    feat: remove initial files and create a new application structure using Tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import { Provider as ReduxProvider } from "react-redux";
-import { TodoList } from "./components/TodoList";
 import { store } from "./store";
-import { AddTodo } from "./components/AddTodo";
+import { Player } from "./pages/Player";
 
 export function App() {
   return (
     <ReduxProvider store={store}>
-      <AddTodo />
-      <TodoList />
+      <Player />
     </ReduxProvider>
   )
 }
