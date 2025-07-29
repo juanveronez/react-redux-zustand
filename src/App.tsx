6ae6bd3b11e59077def556deb79d7f70a6837b9d
Juan Veronez <juan.monteirov@gmail.com>
2025-07-28 22:46:57 -0300

    feat: use `useSelector` to get todo reducer of global store

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,11 @@
+import { Provider as ReduxProvider } from "react-redux";
+import { TodoList } from "./components/TodoList";
+import { store } from "./store";
+
 export function App() {
-  return 'Hello World'
+  return (
+    <ReduxProvider store={store}>
+      <TodoList />
+    </ReduxProvider>
+  )
 }
